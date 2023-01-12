8f1de52d5c6255c66586b6ae499e51ca11f7852f
Luccas Mateus <luccasmmg@gmail.com>
2023-01-12 10:50:30 -0300

    feat: telescope

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -69,6 +69,10 @@ return packer.startup(function(use)
   use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer 
   use "jose-elias-alvarez/null-ls.nvim" -- LSP diagnostics and code actions
 
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+  use "nvim-telescope/telescope-media-files.nvim"
+
   -- Treesitter
   use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
   use {"p00f/nvim-ts-rainbow"}
