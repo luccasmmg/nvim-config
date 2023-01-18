c0e7a8af3e2d8793b7eb54dfa1ee7dcc68c146fa
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2023-01-18 11:39:39 -0300

    feat: added icons

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -78,6 +78,7 @@ return packer.startup(function(use)
   use {"p00f/nvim-ts-rainbow"}
 
   -- Lualine
+  use 'kyazdani42/nvim-web-devicons'
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
