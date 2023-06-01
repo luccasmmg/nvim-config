5182ed9304b594db3637d10451fb0af569893496
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2023-06-01 11:12:10 -0300

    Updates

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -74,4 +74,3 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
-
